4deb60d1491c45c56df3c2b457e2ad4f2e406343
lucas renan <contato@lucasrenan.com>
2016-02-29 19:59:37 -0300

    Create pull requests controller

--- app/server.js
+++ app/server.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import Router from './router'
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
+Router.init(app);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log('Listening...');
