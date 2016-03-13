2a16629a0a5660c626264bb8c0ba6e43541ed9d0
Vitor Talaia <vitor.oliveira2@etec.sp.gov.br>
2016-03-13 14:25:26 -0300

    Add a 30-minute cache between GitHub API calls
    Using [apicache](https://github.com/kwhitley/apicache) module.

--- app/router.js
+++ app/router.js
@@ -1,7 +1,8 @@
+import { middleware as cacheFor } from 'apicache';
 import PullRequestsController from './controllers/pull_requests_controller';
 
 export default class Router {
   static init(app) {
-    app.use('/', PullRequestsController.index)
+    app.use('/', cacheFor('30 minutes'), PullRequestsController.index);
   }
 }
