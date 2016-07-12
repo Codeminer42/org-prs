57107577f703b7f51976b51e9f939d2dd3992665
Elomar Souza <elomar@elomar.me>
2016-07-12 15:56:13 -0300

    Add GithubService basic specs

--- test/services/github_service_spec.js
+++ test/services/github_service_spec.js
@@ -1,5 +1,35 @@
 import { expect } from '../test_helper';
+import nock from 'nock';
+import GithubService from '../../app/services/github_service';
+
+require('dotenv').load();
 
 describe('GithubService', function() {
-  it('must be implemented');
+  describe('openPullRequests', function() {
+    let prs = [{
+      title: "PR 1",
+      html_url: "https://github.com/Codeminer42/org-prs/pull/23",
+      user: { login: "lucasrenan" }
+    }];
+
+    before(function() {
+      nock('https://api.github.com:443', {"encodedQueryParams":true})
+        .get('/orgs/' + process.env.GITHUB_USER + '/repos')
+        .query({"type":"all","per_page":"100","access_token": process.env.OAUTH_TOKEN})
+        .reply(200, [{"id":4252046,"name":"org-prs"}]);
+
+      nock('https://api.github.com:443', {"encodedQueryParams":true})
+        .get('/repos/' + process.env.GITHUB_USER + '/org-prs/pulls')
+        .query({"state": "open", "access_token": process.env.OAUTH_TOKEN})
+        .reply(200, prs);
+    });
+
+    it('returns all open requests', function(done) {
+      GithubService.openPullRequests(function(err, prs) {
+        expect(nock.isDone()).to.be.true;
+        expect(prs).to.eql(prs);
+        done();
+      });
+    });
+  });
 });
