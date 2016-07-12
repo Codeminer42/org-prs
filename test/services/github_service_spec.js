ad7462101f8700ead5212f0761c38d048f555806
Elomar Souza <elomar@elomar.me>
2016-07-12 17:59:26 -0300

    Test GithubService by stubbing api instead of faking HTTP requests

--- test/services/github_service_spec.js
+++ test/services/github_service_spec.js
@@ -1,33 +1,77 @@
 import { expect } from '../test_helper';
-import nock from 'nock';
-import GithubService from '../../app/services/github_service';
-
-require('dotenv').load();
+import mockery from 'mockery';
+import sinon from 'sinon';
 
 describe('GithubService', function() {
   describe('openPullRequests', function() {
-    let prs = [{
-      title: "PR 1",
-      html_url: "https://github.com/Codeminer42/org-prs/pull/23",
-      user: { login: "lucasrenan" }
-    }];
+    let service;
+
+    let client = {
+      repos: {
+        getFromOrg(args, callback) {
+          callback(null, [{name: 'repo-name'}]);
+        }
+      },
+      pullRequests: {
+        getAll(args, callback) {
+          callback(null, [{
+            title: 'PR 1',
+            html_url: 'https://github.com/Codeminer42/org-prs/pull/23',
+            user: { login: 'lucasrenan' }
+          }]);
+        }
+      },
+      authenticate() {}
+    };
 
     before(function() {
-      nock('https://api.github.com:443', {"encodedQueryParams":true})
-        .get('/orgs/' + process.env.GITHUB_USER + '/repos')
-        .query({"type":"all","per_page":"100","access_token": process.env.OAUTH_TOKEN})
-        .reply(200, [{"id":4252046,"name":"org-prs"}]);
-
-      nock('https://api.github.com:443', {"encodedQueryParams":true})
-        .get('/repos/' + process.env.GITHUB_USER + '/org-prs/pulls')
-        .query({"state": "open", "access_token": process.env.OAUTH_TOKEN})
-        .reply(200, prs);
+      mockery.enable({ warnOnUnregistered: false });
+      mockery.registerMock('github', () => client);
+ 
+      service = require('../../app/services/github_service').default;
     });
 
-    it('returns all open requests', function(done) {
-      GithubService.openPullRequests(function(err, prs) {
-        expect(nock.isDone()).to.be.true;
-        expect(prs).to.eql(prs);
+    it('authenticates', function() {
+      let stub = sinon.stub(client, 'authenticate');
+      service.openPullRequests();
+      expect(stub.called).to.be.true;
+    });
+
+    it('checks all repos', function(done) {
+      let spy = sinon.spy(client.repos, 'getFromOrg');
+
+      service.openPullRequests(function(err, prs) {
+        expect(spy.called).to.be.true;
+
+        let argsFirstCall = spy.args[0][0];
+        expect(argsFirstCall.type).to.eql('all');
+
+        done();
+      });
+    });
+
+    it('checks open PRs', function(done) {
+      let spy = sinon.spy(client.pullRequests, 'getAll');
+
+      service.openPullRequests(function(err, prs) {
+        expect(spy.called).to.be.true;
+
+        let argsFirstCall = spy.args[0][0];
+        expect(argsFirstCall.state).to.eql('open');
+
+        done();
+      });
+    });
+
+    it('formats PR', function(done) {
+      service.openPullRequests(function(err, prs) {
+        expect(prs).to.eql([{
+          repo: 'repo-name',
+          title: 'PR 1',
+          url: 'https://github.com/Codeminer42/org-prs/pull/23',
+          user: 'lucasrenan'
+        }]);
+
         done();
       });
     });
